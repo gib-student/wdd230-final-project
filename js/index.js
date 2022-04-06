75650996a293a525fa5042797403ea2a925551f9
gib-student <gib19021@byui.edu>
2022-04-06 14:04:45 -0600

    Update

--- js/index.js
+++ js/index.js
@@ -1,13 +1,3 @@
 // Copyright year
 const date = new Date();
 document.querySelector('#copyright-year').innerHTML = date.getFullYear().toString();
-
-// Topnav
-document.querySelector('.icon').onclick = function() {
-    var x = document.querySelector('#links');
-    if (x.style.display === 'block') {
-        x.style.display = 'none';
-    } else {
-        x.style.display = 'block';
-    }
-};
